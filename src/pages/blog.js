218943ca23f4ce6c936ccabf5d6196c323f4088f
psstory <psstory@yahoo.com>
2019-08-05 15:41:12 -0400

    final gatsby site

---
+++ src/pages/blog.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+import Layout from "../components/layout"
+import blogStyles from "./blog.module.scss"
+import Head from "../components/head"
+
+//
+// Goal: Render Contentful Posts
+//
+// 1. Swap out the markdown query with the contentful query
+// 2. Update the component to render the new data
+//    - Don't worry if the link links to a non-existant page
+// 3. Test your work!
+
+const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: DESC }) {
+        edges {
+          node {
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Head title="Blog" />
+      <h1>Blog</h1>
+      <ol className={blogStyles.posts}>
+        {data.allContentfulBlogPost.edges.map(edge => {
+          return (
+            <li className={blogStyles.post}>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogPage
